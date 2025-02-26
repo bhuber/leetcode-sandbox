b73c6548711e39397d9aaa99e41294bb99dc5c71
Bennet Huber <bennet.huber@gmail.com>
2025-02-25 16:01:00 -0800

    Add a bunch of graph stuff

--- python/leetcode-python/graphs.py
+++ python/leetcode-python/graphs.py
@@ -1,7 +1,12 @@
+import functools
+import heapq
+import itertools
 from collections import defaultdict
+from functools import cache
 from itertools import chain
 from numbers import Number
-from typing import Any, Tuple, List
+from typing import Any, Tuple, List, Iterable
+
 
 class Node:
     def __init__(self, id: Any, data: Any = None, edges: List[Tuple[Any, Number]] = None):
@@ -37,22 +42,146 @@ class ComponentSets:
             self.parents[re1] = re2
             self.rank[re2] += self.rank[re1]
 
+class Colors:
+    WHITE = "WHITE"
+    GRAY = "GRAY"
+    BLACK = "BLACK"
 
-class UndirectedGraph:
-    def __init__(self, edgelist: List[Tuple[Any, Any, Number]]):
-        self.edgelist = edgelist
-        self.nodes = set(chain((e[0] for e in edgelist), (e[1] for e in edgelist)))
+def flatten(list_of_lists):
+    return list(itertools.chain.from_iterable(list_of_lists))
 
-    def __getitem__(self, key):
-        if key not in self.nodes:
-            raise KeyError(f"Node {key} not found in graph.")
-        return Node(key, edges=[(e[1], e[2]) for e in self.edgelist if e[0] == key])
+@functools.total_ordering
+class Edge[T]:
+    def __init__(self, start: T, end: T, weight: Number):
+        self.start = start
+        self.end = end
+        self.weight = weight
+
+    def __lt__(self, other):
+        return isinstance(other, Edge) and self.weight < other.weight
+
+    def __eq__(self, other):
+        return isinstance(other, Edge) and (
+            self.start == other.start and
+            self.end == other.end and
+            self.weight == other.weight)
+
 
     def __repr__(self):
-        return f"UndirectedGraph({self.edgelist})"
+        return f"Edge: {self.start}->{self.end}, {self.weight}"
 
     def __str__(self):
         return self.__repr__()
 
-    def kruskal(self, start):
-        pass
+
+class Graph[T]:
+    def __init__(self, edgelist: Iterable [Tuple[T, T, Number]], directed=True):
+        self.directed = directed
+        """
+        self.edgelist: List[Edge] = sorted(Edge(e[0], e[1], e[2]) for e in (
+            set(edgelist)
+            if directed else
+            set(chain(edgelist, ((e[1], e[0], e[2]) for e in edgelist)))))
+        """
+        self.edgelist: List[Edge] = sorted(Edge(e[0], e[1], e[2]) for e in (set(edgelist)))
+        self.nodes = set(chain((e[0] for e in edgelist), (e[1] for e in edgelist)))
+
+    def __getitem__(self, key: T):
+        if key not in self.nodes:
+            raise KeyError(f"Node {key} not found in graph.")
+        return Node(key, edges=[(e[1], e[2]) for e in self.edgelist if e[0] == key])
+
+    @cache
+    def neighbors(self, n: T) -> List[T]:
+        result = [e.end for e in self.edgelist if e.start == n]
+        if not self.directed:
+            result += [e.start for e in self.edgelist if e.end == n]
+        return result
+
+    @cache
+    def out_edges(self, n: T) -> List[Edge[T]]:
+        result = [e for e in self.edgelist if e.start == n]
+        if not self.directed:
+            result += [Edge(e.end, e.start, e.weight) for e in self.edgelist if e.end == n]
+        return result
+
+
+    def __repr__(self):
+        return ("DirectedGraph" if self.directed else "UndirectedGraph") + f"({self.edgelist})"
+
+    def __str__(self):
+        return self.__repr__()
+
+    def dfs(self) -> List[List[T]]:
+        colors: defaultdict[T, str] = defaultdict(lambda: Colors.WHITE)
+
+        def dfs_visit(n: T) -> List[T]:
+            if colors[n] == Colors.WHITE:
+                colors[n] = Colors.GRAY
+                result = [n] + flatten(dfs_visit(neighbor) for neighbor in self.neighbors(n))
+                colors[n] = Colors.BLACK
+                return result
+            else:
+                return []
+
+        result = []
+        for n in sorted(self.nodes):
+            if colors[n] == Colors.WHITE:
+                result.append(dfs_visit(n))
+        return result
+
+
+    def kruskal(self) -> List[Edge[T]]:
+        edges = [e for e in self.edgelist]
+        heapq.heapify(edges)
+        subcomponents = ComponentSets()
+        mst = []
+        for n in self.nodes:
+            subcomponents.add(n)
+
+        while len(mst) < (len(self.nodes) - 1) and edges:
+            e = heapq.heappop(edges)
+            assert e
+            if subcomponents.representative(e.start) != subcomponents.representative(e.end):
+                mst.append(e)
+                subcomponents.union(e.start, e.end)
+
+        return mst
+
+    def prim(self, start=None) -> List[Edge[T]]:
+        if start is None:
+            start = self.nodes[0]
+        result = []
+        in_mst = set(start)
+        next_edges = self.out_edges(start)
+        heapq.heapify(next_edges)
+
+        while len(result) < (len(self.nodes) - 1) and next_edges:
+            e = heapq.heappop(next_edges)
+            if e.end in in_mst:
+                continue
+            result.append(e)
+            in_mst.add(e.end)
+            for neighbor_edge in self.out_edges(e.end):
+                if neighbor_edge.end not in in_mst:
+                    heapq.heappush(next_edges, neighbor_edge)
+
+        return result
+
+    def check_mst(self, tree):
+        edges = [e for e in self.edgelist]
+        heapq.heapify(edges)
+        min_edges = heapq.nsmallest(len(self.nodes) - 1, edges)
+        min_mst_weight = sum(e.weight for e in min_edges)
+        spanned_nodes = sorted(set(flatten((e.start, e.end) for e in tree)))
+        is_full_span = len(self.nodes) == len(spanned_nodes)
+        mst_weight = sum(e.weight for e in tree)
+
+        return {
+            'min_mst_weight': min_mst_weight,
+            'spanned_nodes': spanned_nodes,
+            'n_spanned_nodes': len(spanned_nodes),
+            'is_full_span': is_full_span,
+            'mst_weight': mst_weight,
+        }
+
--- python/leetcode-python/test_graphs.py
+++ python/leetcode-python/test_graphs.py
@@ -1,48 +1,154 @@
 from unittest import TestCase
 
-from graphs import ComponentSets
+from graphs import ComponentSets, Graph
+
+
+def udg_1():
+    return [
+        ('a', 'b', 4),
+        ('a', 'h', 8),
+        ('b', 'c', 8),
+        ('b', 'h', 11),
+        ('c', 'i', 2),
+        ('c', 'f', 4),
+        ('c', 'd', 7),
+        ('d', 'e', 9),
+        ('d', 'f', 14),
+        ('e', 'f', 10),
+        ('f', 'g', 2),
+        ('g', 'i', 6),
+        ('g', 'h', 1),
+        ('h', 'i', 7),
+    ]
+
+
+def dg_1():
+    return [(x[0], x[1], 1) for x in [
+        ('a', 'b'),
+        ('b', 'c'),
+        ('b', 'e'),
+        ('b', 'f'),
+        ('c', 'd'),
+        ('c', 'g'),
+        ('d', 'c'),
+        ('d', 'h'),
+        ('e', 'a'),
+        ('e', 'f'),
+        ('f', 'g'),
+        ('g', 'f'),
+        ('g', 'h'),
+        ('h', 'h'),
+    ]]
+
+
+def dg_2():
+    return [(x[0], x[1], 1) for x in [
+        ('a', 'b'),
+        ('b', 'e'),
+        ('c', 'd'),
+        ('d', 'c'),
+        ('e', 'a'),
+        ('f', 'g'),
+        ('g', 'f'),
+        ('h', 'h'),
+    ]]
 
 
 class TestComponentSets(TestCase):
-    undertest = ComponentSets()
-    def test_basic(self):
-        self.undertest.add(1)
-        self.undertest.add(2)
-        self.assertEqual(1, self.undertest.representative(1))
-        self.assertEqual(2, self.undertest.representative(2))
+    ut = ComponentSets()
 
-        self.undertest.union(1, 2)
-        self.assertEqual(self.undertest.representative(1), self.undertest.representative(2))
+    def test_basic(self):
+        self.ut.add(1)
+        self.ut.add(2)
+        self.assertEqual(1, self.ut.representative(1))
+        self.assertEqual(2, self.ut.representative(2))
+
+        self.ut.union(1, 2)
+        self.assertEqual(self.ut.representative(1), self.ut.representative(2))
 
     def test_union(self):
         for i in range(0, 10):
-            self.undertest.add(i)
+            self.ut.add(i)
 
         for i in range(0, 10):
-            self.assertEqual(i, self.undertest.representative(i))
+            self.assertEqual(i, self.ut.representative(i))
 
         for i in range(1, 10):
-            self.undertest.union(1, i)
+            self.ut.union(1, i)
 
-        distinct_components = set(self.undertest.representative(i) for i in range(1, 10))
+        distinct_components = set(self.ut.representative(i) for i in range(1, 10))
         self.assertEqual(1, len(distinct_components))
 
     def test_union_2(self):
         for i in range(0, 10):
-            self.undertest.add(i)
+            self.ut.add(i)
 
         for i in range(1, 4):
-            self.undertest.union(0, i)
+            self.ut.union(0, i)
         for i in range(5, 7):
-            self.undertest.union(4, i)
-        self.undertest.union(7, 8)
+            self.ut.union(4, i)
+        self.ut.union(7, 8)
 
-        distinct_components = set(self.undertest.representative(i) for i in range(1, 10))
+        distinct_components = set(self.ut.representative(i) for i in range(1, 10))
         self.assertEqual(4, len(distinct_components))
 
-        self.undertest.union(3, 4)
-        self.undertest.union(9, 8)
-        self.undertest.union(7, 5)
+        self.ut.union(3, 4)
+        self.ut.union(9, 8)
+        self.ut.union(7, 5)
 
-        distinct_components = set(self.undertest.representative(i) for i in range(1, 10))
+        distinct_components = set(self.ut.representative(i) for i in range(1, 10))
         self.assertEqual(1, len(distinct_components))
+
+
+class TestGraphs(TestCase):
+    def test_dfs1(self):
+        ut = Graph(udg_1(), False)
+        dfs = ut.dfs()
+        print(dfs)
+        self.assertEqual(1, len(dfs))
+        self.assertEqual(9, len(set(dfs[0])))
+
+    def test_dfs2(self):
+        ut = Graph(dg_1(), True)
+        dfs = ut.dfs()
+        print(dfs)
+        self.assertEqual(1, len(dfs))
+        self.assertEqual(8, len(set(dfs[0])))
+
+    def test_dfs3(self):
+        ut = Graph(dg_2(), True)
+        dfs = ut.dfs()
+        print(dfs)
+        expected = sorted(sorted(sc) for sc in [['a', 'b', 'e'], ['f', 'g'], ['d', 'c'], ['h']])
+        self.assertEqual(4, len(dfs))
+        self.assertEqual(expected, sorted(sorted(tree) for tree in dfs))
+
+    def test_kruskal(self):
+        ut = Graph(udg_1(), False)
+        mst = ut.kruskal()
+
+        print(mst)
+        self.assertEqual(8, len(mst))
+
+        expected_mst_check = {
+            'min_mst_weight': 33,
+            'spanned_nodes': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
+            'n_spanned_nodes': 9,
+            'is_full_span': True,
+            'mst_weight': 37}
+        self.assertEqual(expected_mst_check, ut.check_mst(mst))
+
+    def test_prim(self):
+        ut = Graph(udg_1(), False)
+        mst = ut.prim('a')
+
+        print(mst)
+        self.assertEqual(8, len(mst))
+
+        expected_mst_check = {
+            'min_mst_weight': 33,
+            'spanned_nodes': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
+            'n_spanned_nodes': 9,
+            'is_full_span': True,
+            'mst_weight': 37}
+        self.assertEqual(expected_mst_check, ut.check_mst(mst))
